ce4db47c2f19f1335787284e57b45314655eed59
Alan-H-H <123487644+Alan-H-H@users.noreply.github.com>
2024-08-09 20:30:56 -0300

    commit 8__9

--- src/App.js
+++ src/App.js
@@ -1,85 +1,78 @@
-import logo from './logo.svg';
+
 import './App.css'
 import BackgroundSVG from "./images/background-pattern-desktop.svg"
 import IconSVG from "./images/icon-star.svg"
 import PlusSVG from "./images/icon-plus.svg"
 import MinusSVG from "./images/icon-minus.svg"
 import mobileSVG from "./images/background-pattern-mobile.svg"
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  let [plusone, setPlusone] = useState("plus__hide")
+  let [plustwo, setPlustwo] = useState("plus__hide")
+  let [plusthree, setPlusthree] = useState("plus__hide")
+  let [plusfour, setPlusfour] = useState("plus__hide")
+  let [minusone, setMinusone] = useState("minus__show")
+  let [minustwo, setMinustwo] = useState("minus__show")
+  let [minusthree, setMinusthree] = useState("minus__show")
+  let [minusfour, setMinusfour] = useState("minus__show")
 
-  function handleClick1(){
-    let faq1 = document.getElementById("faq1")
-    let plus1 = document.querySelector(".plus1")
-    let minus1 = document.querySelector(".minus1")
-    
-    if (faq1.classList.contains("faq1")){
-      faq1.className = 'faq1__hide'
-      plus1.style.display = 'flex'
-      minus1.style.display = 'none'
-    }
+  let [change, setChange ] = useState("faq")
+  let [reuse, setReuse] = useState("faq")
+  let [reset, setReset] = useState("faq")
+  let [insert, setInsert] = useState("faq")
 
-    else{
-      faq1.className = 'faq1'
-      plus1.style.display = 'none'
-      minus1.style.display = 'flex'
-    }
+  const toggleState = (state, setState, hideValue, showValue) => {
+    setState(state === hideValue ? showValue : hideValue);
   }
-
-  function handleClick2(){
-    let faq2 = document.getElementById("faq2")
-    let plus2 = document.querySelector(".plus2")
-    let minus2 = document.querySelector(".minus2")
+    const handlePlusoneToggle = () => {
+      toggleState(plusone, setPlusone, "plus__hide", "plus__show");
+    };
+    const handleMinusoneToggle = () => {
+      toggleState(minusone, setMinusone, "minus__hide", "minus__show");
+    };
+    const handlePlustwoToggle = () => {
+      toggleState(plustwo, setPlustwo, "plus__hide", "plus__show");
+    };
+    const handleMinustwoToggle = () => {
+      toggleState(minustwo, setMinustwo, "minus__hide", "minus__show");
+    };
+    const handlePlusthreeToggle = () => {
+      toggleState(plusthree, setPlusthree, "plus__hide", "plus__show");
+    };
+    const handleMinusthreeToggle = () => {
+      toggleState(minusthree, setMinusthree, "minus__hide", "minus__show");
+    };
+    const handlePlusfourToggle = () => {
+      toggleState(plusfour, setPlusfour, "plus__hide", "plus__show");
+    };
+    const handleMinusfourToggle = () => {
+      toggleState(minusfour, setMinusfour, "minus__hide", "minus__show");
+    };
     
-    if (faq2.classList.contains("faq2")){
-      faq2.className = 'faq2__hide'
-      plus2.style.display = 'flex'
-      minus2.style.display = 'none'
-    }
+    const handleChangeToggle = () => {
+      toggleState(change, setChange, "faq", "faq__hide");
+      handlePlusoneToggle();
+      handleMinusoneToggle() 
+    };
 
-    else{
-      faq2.className = 'faq2'
-      plus2.style.display = 'none'
-      minus2.style.display = 'flex'
-    }
-  }
+    const handleReuseToggle = () => {
+      toggleState(reuse, setReuse, "faq", "faq__hide");
+      handlePlustwoToggle()
+      handleMinustwoToggle()
+    };
 
-  function handleClick3(){
-    let faq3 = document.getElementById("faq3")
-    let plus3 = document.querySelector(".plus3")
-    let minus3 = document.querySelector(".minus3")
-    
-    if (faq3.classList.contains("faq3")){
-      faq3.className = 'faq3__hide'
-      plus3.style.display = 'flex'
-      minus3.style.display = 'none'
-    }
-
-    else{
-      faq3.className = 'faq3'
-      plus3.style.display = 'none'
-      minus3.style.display = 'flex'
-    }
-  }
-
-  function handleClick4(){
-    let faq4 = document.getElementById("faq4")
-    let plus4 = document.querySelector(".plus4")
-    let minus4 = document.querySelector(".minus4")
-    
-    if (faq4.classList.contains("faq4")){
-      faq4.className = 'faq4__hide'
-      plus4.style.display = 'flex'
-      minus4.style.display = 'none'
-    }
-
-    else{
-      faq4.className = 'faq4'
-      plus4.style.display = 'none'
-      minus4.style.display = 'flex'
-    }
-  }
+    const handleResetToggle = () => {
+      toggleState(reset, setReset, "faq", "faq__hide");
+      handlePlusthreeToggle()
+      handleMinusthreeToggle()
+    };
+  
+    const handleInsertToggle = () => {
+      toggleState(insert, setInsert, "faq", "faq__hide");
+      handlePlusfourToggle()
+      handleMinusfourToggle()
+    };
   
   return (
 <div className="container__main__up">
@@ -90,44 +83,44 @@ function App() {
       <img src={IconSVG} alt="" width= "50px"/>
         <h1>FAQs</h1>
     </div>
-      <button className="container__faq1" onClick={handleClick1}>
+      <button className="container__faq" onClick={handleChangeToggle}>
         <div className='align__btns'>
-          <h2>What is Frontend Mentor, and how will it help me?</h2>
-            <img className="plus1" src={PlusSVG} alt="" width= "25px"/>
-            <img className="minus1" src={MinusSVG} alt="" width= "25px"/>
+          <h2 >What is Frontend Mentor, and how will it help me?</h2>
+            <img className={plusone} src={PlusSVG} alt="" width= "25px"/>
+            <img className={minusone} src={MinusSVG} alt="" width= "25px"/>
         </div>
-              <p className='faq1' id="faq1">Frontend Mentor offers realistic coding challenges to help developers improve their 
+              <p class={change} id={change}>Frontend Mentor offers realistic coding challenges to help developers improve their 
               frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for 
               all levels and ideal for portfolio building.</p>
       </button>
       <hr/>
-      <button className="container__faq2" onClick={handleClick2}>
+      <button className="container__faq" onClick={handleReuseToggle}>
         <div className='align__btns'>
-          <h2>Is Frontend Mentor free?</h2>
-            <img className="plus2" src={PlusSVG} alt="" width= "25px"/>
-            <img className="minus2" src={MinusSVG} alt="" width= "25px"/>
+          <h2 className='padd__icon'>Is Frontend Mentor free?</h2>
+            <img className={plustwo} src={PlusSVG} alt="" width= "25px"/>
+            <img className={minustwo} src={MinusSVG} alt="" width= "25px"/>
         </div>
-          <p className='faq2' id="faq2">Yes, Frontend Mentor offers both free and premium coding challenges, with the free 
+          <p className={reuse} id={reuse}>Yes, Frontend Mentor offers both free and premium coding challenges, with the free 
           option providing access to a range of projects suitable for all skill levels.</p>
       </button>
       <hr/>
-      <button className="container__faq3" onClick={handleClick3}>
+      <button className="container__faq" onClick={handleResetToggle}>
         <div className='align__btns'>
           <h2>Can I use Frontend Mentor projects in my portfolio?</h2>
-            <img className="plus3" src={PlusSVG} alt="" width= "25px"/>
-            <img className="minus3" src={MinusSVG} alt="" width= "25px"/>
+            <img className={plusthree} src={PlusSVG} alt="" width= "25px"/>
+            <img className={minusthree} src={MinusSVG} alt="" width= "25px"/>
         </div>
-          <p className='faq3' id="faq3">Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
+          <p className={reset} id={reset}>Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
           way to showcase your skills to potential employers!</p>
       </button>
       <hr/>
-      <button className="container__faq4" onClick={handleClick4}>
+      <button className="container__faq" onClick={handleInsertToggle}>
         <div className='align__btns'>
           <h2>How can I get help if I'm stuck on a Frontend Mentor challenge?</h2>
-            <img className="plus4" src={PlusSVG} alt="" width= "25px"/>
-            <img className="minus4" src={MinusSVG} alt="" width= "25px"/>
+            <img className={plusfour} src={PlusSVG} alt="" width= "25px"/>
+            <img className={minusfour} src={MinusSVG} alt="" width= "25px"/>
         </div>
-          <p className='faq4' id="faq4">The best place to get help is inside Frontend Mentor's Discord community. There's a help 
+          <p className={insert} id={insert}>The best place to get help is inside Frontend Mentor's Discord community. There's a help 
           channel where you can ask questions and seek support from other community members.</p>
       </button>
       <div className="attribution">
